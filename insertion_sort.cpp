e653f75a66be7ff1c001296d10ef7ceb6cbae90a
Shobi P P <ppshobi@gmail.com>
2016-07-03 13:22:31 +0530

    refactored insertion sort

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,7 +1,6 @@
 #include<iostream.h>
 #include<conio.h>
-class insertion
-{
+class insertion{
 	int s,i,j;
 	int arr[20],t[20];
 	public:
@@ -9,27 +8,20 @@ class insertion
 	void sorting();
 	void display();
 };
-void insertion::read()
-{
+void insertion::read(){
 	cout<<"\nEnter the size:";
 	cin>>s;
 	cout<<"\nEnter the elements:";
-	for(i=0;i<s;i++)
-	{
+	for(i=0;i<s;i++){
 		cin>>arr[i];
 	}
 }
-void insertion::sorting()
-{
-	for(i=1;i<s;i++)
-	{
-		for(j=0;j<i;j++)
-		{
-			if(arr[i]<arr[j])
-			{
+void insertion::sorting(){
+	for(i=1;i<s;i++){
+		for(j=0;j<i;j++){
+			if(arr[i]<arr[j]){
 				t[i]=arr[i];
-				for(int k=i;k>j;k--)
-				{
+				for(int k=i;k>j;k--){
 					arr[k]=arr[k-1];
 				}
 				arr[j]=t[i];
@@ -37,16 +29,13 @@ void insertion::sorting()
 		}
 	}
 }
-void insertion::display()
-{
+void insertion::display(){
 	cout<<"\nThe sorted array:\n\n";
-	for(i=0;i<s;i++)
-	{
+	for(i=0;i<s;i++){
 		cout<<arr[i]<<"\t";
 	}
 }
-void main()
-{
+void main(){
 	insertion i;
 	clrscr();
 	i.read();
