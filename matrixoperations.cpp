c42f10161ec7f221d7503a5511f167a25c6b5c2a
Shobi P P <ppshobi@gmail.com>
2016-07-03 12:54:22 +0530

    Matrix operatios refactored

--- matrixoperations.cpp
+++ matrixoperations.cpp
@@ -5,30 +5,31 @@ int r1,r2,c1,c2,i,j,k,a[20][20],b[20][20],c[20][20];
   public:               
     matrix();                       
     void add();                       
-    void sub();                       
-    void div();                       
+    void sub();                                            
     void mult();               
 };
 matrix::matrix(){    
-  cout << "enter the order of the 1st matix";    
-  cin >> r1 >> c1;    
-  cout << "enter elements in the 1st matrix";    
+  cout << "Enter the order of the 1st matix\n";    
+  cin >> r1;
+  cin >> c1;    
+  cout << "Enter elements in the 1st matrix\n";    
   for (i = 0; i < r1; i++)  
-  for (j = 0; j < c1; j++)      
-  cin >> a[i][j];    
-  cout << "enter the order of the 2nd matrix";    
-  cin >> r2 >> c2;    
-  cout << "enter the elments";    
+  	for (j = 0; j < c1; j++)      
+  		cin >> a[i][j];    
+  cout << "Enter the order of the 2nd matrix\n";    
+  	cin >> r2;
+  	cin >> c2;    
+  cout << "Enter the elments of 2nd Matrix\n";    
   for (i = 0; i < r2; i++)  
-  for (j = 0; j < c2; j++)      
-  cin >> b[i][j];
+  	for (j = 0; j < c2; j++)      
+ 		cin >> b[i][j];
 }
 void matrix::add(){    
 if (r1 == r2 && c1 == c2){
   for (i = 0; i < r2; i++)      
     for (j = 0; j < c2; j++)    
       c[i][j] = a[i][j] + b[i][j];  
-  cout << "The Resultannt Matrix in addition is\n";  
+cout << "The Resultant Matrix in addition is\n";  
   for (i = 0; i < r2; i++){      
     for (j = 0; j < c2; j++){
       cout << c[i][j] << '\t';    
@@ -55,21 +56,6 @@ if (r1 == r2 && c1 == c2){
   cout << "matrix can not be substracted";
 }
 
-void matrix::div(){    
-if (r1 == r2 && c1 == c2){  
-  for (i = 0; i < r2; i++)      
-    for (j = 0; j < c2; j++)    
-      c[i][j] = a[i][j] / b[i][j];
-  cout << "The Resultannt Matrix in division is\n";  
-  for (i = 0; i < r2; i++){
-    for (j = 0; j < c2; j++){
-      cout << c[i][j] << '\t';    
-  }      
-  cout << "\n";     
-  }  
-}else
-  cout << "matrix can not be divided";
-}
 
 void matrix::mult(){
 if (c1 == r2){
@@ -80,8 +66,8 @@ if (c1 == r2){
         c[i][j] += a[i][k] * b[k][j];        
       }   
     }      
-  }  
-  cout << "The Resultannt Matrix in multiplication is\n"; 
+ }  
+cout << "The Resultant Matrix in Multiplication is\n"; 
   for (i = 0; i < r1; i++){
     for (j = 0; j < c2; j++){
       cout << c[i][j] << '\t';
@@ -101,13 +87,12 @@ int main(){
   int ch;
   char ch1;
   do{
-    cout <<"\nMenu\n1.Addition\n2.Substraction\n3.Division\n4.Multiplication\nEnter your choice";
+    cout <<"\nMenu\n1.Addition\n2.Substraction\n3.Multiplication\nEnter your choice";
     cin >> ch; 
     switch (ch){
       case 1:m.add();break;
       case 2:m.sub();break;
-      case 3:m.div();break;
-      case 4:m.mult();break;
+      case 3:m.mult();break;
       default:break;     
     }
     cout << "\nDo You Want To Continue(y/n)";  
