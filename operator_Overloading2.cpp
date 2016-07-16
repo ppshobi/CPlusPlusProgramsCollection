6026c37b4dbf36d8deb77370bc0c508b09407cbd
Shobi P P <ppshobi@gmail.com>
2016-07-16 13:22:53 +0530

    Operator Overloading done

--- operator_Overloading2.cpp
+++ operator_Overloading2.cpp
@@ -4,8 +4,8 @@
 class complex{
   int a,b;
   public:
-    complex() {
-      cout<<"Enter the value of a Complex Number a,b:";
+    void getval(){
+      cout<<"\nEnter the value of a Complex Number a,b:\n";
       cin>>a>>b;
     }
     complex operator+(complex obj){
@@ -21,14 +21,19 @@ class complex{
 
 int main()
 {
-   clrscr();
-   complex obj1,obj2, result;
-   result = obj1+obj2; 
-   cout<<"Your Complex Numbers:\n";
-   obj1.display();
-   obj2.display();  
-   cout<<"Result of addition:";
-   result.display(); 
-   getch();
-   return 0;
+  clrscr();
+  cout<<"\n===========================================\n";
+  cout<<"Program to Demonstrate Operator Overloading\n";
+  cout<<"\n===========================================\n";
+  complex obj1,obj2, result;
+  obj1.getval();
+  obj2.getval();
+  result = obj1+obj2; 
+  cout<<"Your Complex Numbers:\n";
+  obj1.display();
+  obj2.display();  
+  cout<<"Result of addition:";
+  result.display(); 
+  getch();
+  return 0;
 }
