fbd3f13d5961cc8ab5371a3818522974101b7388
Shobi P P <ppshobi@gmail.com>
2016-07-16 13:44:55 +0530

    Virtual base class completed

--- vitual_base_class.cpp
+++ vitual_base_class.cpp
@@ -1,33 +1,36 @@
 #include <iostream.h>
 #include <conio.h>
 class A {
-   public: 
-       int i; 
+  public: 
+    int i; 
 };
-
 class B : virtual public A {
-   public: 
-       int j; 
+  public: 
+    int j; 
 };
-
 class C: virtual public A {
-   public: 
-       int k; 
+  public: 
+    int k; 
 };
-
 class D: public B, public C {
-   public: 
-       int sum; 
+  public: 
+    int sum; 
 };
-
 int main() {
-   D ob; 
-   ob.i = 10; //unambiguous since only one copy of i is inherited. 
-   ob.j = 20; 
-   ob.k = 30; 
-   ob.sum = ob.i + ob.j + ob.k; 
-   cout << “Value of i is : ”<< ob.i<<”\n”; 
-   cout << “Value of j is : ”<< ob.j<<”\n”; cout << “Value of k is :”<< ob.k<<”\n”; 
-   cout << “Sum is : ”<< ob.sum <<”\n”; 
-   return 0; 
+  clrscr();
+  cout<<"\n===========================================\n";
+  cout<<"Program to Demonstrate Virtual Base Classes\n";
+  cout<<"===========================================\n";
+  D ob; 
+  ob.i = 10; //unambiguous since only one copy of i is inherited. 
+  ob.j = 20; 
+  ob.k = 30; 
+  ob.sum = ob.i + ob.j + ob.k; 
+  cout << "Value of i is : "<< ob.i<<"\n"; 
+  cout << "Value of j is : "<< ob.j<<"\n"; 
+  cout << "Value of k is :"<< ob.k<<"\n"; 
+  cout << "Sum is : "<< ob.sum <<"\n"; 
+  getch();
+  return 0; 
 }
+
